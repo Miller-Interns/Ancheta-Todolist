89404ecfebf6f7027a6b7a09d1e9700038058ace
HeReads <raingancheta@su.edu.ph>
2025-12-03 11:57:02 +0800

    refactor/updates-based-on-suggestions

--- src/types/Category.ts
+++ src/types/Category.ts
@@ -2,7 +2,7 @@ import type { TodoItem } from './TodoItem.ts';
 
 
 export interface Category {
-id: string;
-title: string;
-todos: TodoItem[];
+    id: string;
+    title: string;
+    todos: TodoItem[];
 }
