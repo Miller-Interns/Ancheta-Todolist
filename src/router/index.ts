3e9e338d10270f93666204901c9b3f9c2febfd2b
HeReads <raingancheta@su.edu.ph>
2025-12-03 14:47:17 +0800

    refactor/Using-enum-in-router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,9 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 import homePage from '@/pages/home-page.vue';
 import todoApp from '@/pages/todo-app.vue';
 
+enum RouteName {
+    HOME = 'Home',
+    APP = 'App'
+}
+
 const routes = [
-    { path: '/', name: 'Home', component: homePage },
-    { path: '/app', name: 'App', component: todoApp }
+    { path: '/', name: RouteName.HOME, component: homePage },
+    { path: '/app', name: RouteName.APP, component: todoApp }
 ];
 
 export const router = createRouter({
